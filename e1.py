95c9441b1bd5ad0608608c416c8f6e2c4c60489a
gh0stgir1 <145555035+gh0stgir1@users.noreply.github.com>
2024-02-01 12:35:14 +0100

    Update e1.py

--- e1.py
+++ e1.py
@@ -1,1 +1,30 @@
+"""
+Helena Herrera Aviles
+1/2/24
+exercici 1
+"""
+try:
+    def est_paraules(paraules):
+        if not paraules:
+            return None, None, None
+        paraula_llarga = max(paraules, key=len, default="")
+        paraula_curta = min(paraules, key=len, default="")
+        mitjana_mida = sum(len(paraula) for paraula in paraules) / len(paraules)
+        return paraula_llarga, paraula_curta, mitjana_mida
+    def main():
+        paraules = []
 
+        while (paraula := input("Introdueix una paraula (o escriu '\\q' per sortir): ")) != "\\q":
+            paraules.append(paraula)
+
+        paraula_llarga, paraula_curta, mitjana_mida = est_paraules(paraules)
+
+        print("\nResultats:")
+        print(f"Paraula més llarga: {paraula_llarga}")
+        print(f"Paraula més curta: {paraula_curta}")
+        print(f"Mida mitjana de les paraules: {mitjana_mida:.2f}")
+
+    if __name__ == "__main__":
+        main()
+except:
+    print("així no era")
